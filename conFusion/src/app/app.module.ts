4eec2735374552742f8d1d69147f2cbe4cec0af8
spiritxhx <spiritxhx@gmail.com>
2019-03-08 12:20:16 -0500

    Angular 1st modify

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
   providers: [],
